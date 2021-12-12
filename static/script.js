036ab51849f8bd8aa00d7814b1fa194c98c5baaf
izaelgs <izael-123@outlook.com>
2021-12-12 16:51:37 -0300

    menu responsivo

---
+++ static/script.js
@@ -0,0 +1,11 @@
+/*Abre e fecha menu lateral mobile menu pica pra caraio*/
+
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
+
+menuMobile.addEventListener('click', ()=>{
+    menuMobile.classList.contains("bi-list") 
+        ? menuMobile.classList.replace("bi-list", "bi-x")
+        : menuMobile.classList.replace("bi-x", "bi-list");  
+        body.classList.toggle("menu-nav-active");  
+});
