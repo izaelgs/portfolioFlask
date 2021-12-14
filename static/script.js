a000e56592fe45f6325b283e8f37c3e99bbe9efb
izaelgs <izael-123@outlook.com>
2021-12-13 21:49:48 -0300

    habilidades

--- static/script.js
+++ static/script.js
@@ -2,10 +2,21 @@
 
 const menuMobile = document.querySelector('.menu-mobile');
 const body = document.querySelector('body');
+const nav_item = document.querySelectorAll('.nav-item');
+console.log(nav_item)
 
 menuMobile.addEventListener('click', ()=>{
     menuMobile.classList.contains("bi-list") 
         ? menuMobile.classList.replace("bi-list", "bi-x")
         : menuMobile.classList.replace("bi-x", "bi-list");  
         body.classList.toggle("menu-nav-active");  
+});
+
+nav_item.forEach(item => {
+    item.addEventListener('click', ()=>{
+        if(body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list")
+        }
+    })
 });
